2d4f7cd331fc27e92ee444a4b11ae7ba71ec7283
GG <gaiagonen@gmail.com>
2018-10-21 11:02:55 +0300

    feat: Reset button

--- src/Main.elm
+++ src/Main.elm
@@ -5,15 +5,18 @@ import Html.Events exposing (onClick)
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
+  -- MODEL
 
 type alias Model = Int
-
 
 init : Model
 init =
   0
 
-type Msg = Increment | Decrement
+-- MODEL
+
+
+type Msg = Increment | Decrement | Reset
 
 update : Msg -> Model -> Model
 update msg model =
@@ -24,10 +27,17 @@ update msg model =
     Decrement ->
       model - 1
 
+    Reset ->
+       0
+
+-- VIEW
+
+
 view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (String.fromInt model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "reset" ]
     ]
