df5fb3ccf0e833bb75c8e9dd7e58298114a64158
GG <gaiagonen@gmail.com>
2018-10-24 20:59:19 +0300

    dice with images

--- src/Main.elm
+++ src/Main.elm
@@ -1,86 +1,68 @@
 import Browser
 import Html exposing (..)
+import Html.Events exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Random
 
 --MAIN
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
-
--- MODEL
+--MODEL
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  , age : Int
+  { dieFace : Int
   }
 
-init : Model
-init =
-  Model "" "" "" 0
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model 1
+  , Cmd.none
+  )
 
 
--- UPDATE
+
+
+
+--UPDATE
 
 type Msg
-  = Name String
-  | Password String
-  | PasswordAgain String
-  | Age Int
+  = Roll
+  | NewFace Int
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Name name ->
-      { model | name = name }
+    Roll ->
+      ( model
+      , Random.generate NewFace (Random.int 1 6)
+      )
 
-    Password password ->
-      { model | password = password }
+    NewFace newFace ->
+      ( Model newFace
+      , Cmd.none
+      )
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
 
-    Age age ->
-      { model | age = age }
 
--- VIEW
+--SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+--VIEW
 
 view : Model -> Html Msg
 view model =
   div []
-  [ viewInput "text" "Name" model.name Name
-  , viewInput "password" "Password" model.password Password
-  , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-  , viewInput "age" "Age" (String.fromInt model.age) (String.fromInt Age)
-  , viewValidation model
+  [ img [ src <| "src/"++String.fromInt model.dieFace++".png" ] []
+  , button [ onClick Roll ] [ text "Roll" ]
   ]
-
-viewInput : String -> String -> String -> (String -> msg ) -> Html msg
-viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg ] []
-
-viewValidation : Model -> Html msg
-viewValidation model =
-  if passMatchValid model && passLengthValid model then
-    div [ style "color" "green" ] [ text "OK!" ]
-  else if passMatchValid model then
-    div [ style "color" "red" ] [ text "Password must be 8 chars long!" ]
-  else
-    div [ style "color" "red" ] [ text "Passwords do not match!" ]
-
-passMatchValid : Model -> Bool
-passMatchValid model =
-  if model.password == model.passwordAgain then
-    True
-  else
-    False
-
-passLengthValid : Model -> Bool
-passLengthValid model =
-  if String.length model.password > 8 then
-    True
-  else
-    False
